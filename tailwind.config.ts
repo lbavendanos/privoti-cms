a45d6353d0a59221db470f7cba3d50ffa20a1b5a
lbavendanos <lbavendanos@gmail.com>
2024-07-30 21:37:42 -0500

    Fix container class

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,9 +13,12 @@ const config = {
   theme: {
     container: {
       center: true,
-      padding: '2rem',
+      padding: '1rem',
       screens: {
-        '2xl': '1400px',
+        DEFAULT: '640px',
+        sm: '768px',
+        md: '1024px',
+        lg: '1280px',
       },
     },
     extend: {

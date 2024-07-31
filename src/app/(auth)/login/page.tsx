80b29de77456ef6cf8b831db163a04b125a2ba6d
lbavendanos <lbavendanos@gmail.com>
2024-07-30 21:40:51 -0500

    Add login page

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,48 @@
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+export default function LoginPage() {
+  return (
+    <section className="container">
+      <div className="grid grid-cols-12 gap-6">
+        <div className="col-span-12 md:col-span-8 md:col-start-3 lg:col-span-6 lg:col-start-4 xl:col-span-4 xl:col-start-5">
+          <Card className="w-full">
+            <CardHeader>
+              <CardTitle className="text-2xl">Login</CardTitle>
+              <CardDescription>
+                Enter your email below to login to your account.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" required />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full">Sign in</Button>
+            </CardFooter>
+          </Card>
+        </div>
+      </div>
+    </section>
+  )
+}

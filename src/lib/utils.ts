44ac99af5d7786db0df1d7fb6c81b5ff2277041a
lbavendanos <lbavendanos@gmail.com>
2024-07-30 20:44:54 -0500

    Add docs to utils functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-export function cn(...inputs: ClassValue[]) {
+/**
+ * Utility for constructing className strings conditionally and merging them with Tailwind CSS classes.
+ *
+ * @param {ClassValue[]} inputs - The classes to add or merged.
+ * @returns {string} Returns a string of class names.
+ */
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs))
 }

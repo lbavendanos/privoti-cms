4a11722de53f09efca6b058f44925a9057a59cbc
lbavendanos <lbavendanos@gmail.com>
2024-07-31 10:53:02 -0500

    Add login form

---
+++ src/app/(auth)/login/_components/login-form.tsx
@@ -0,0 +1,97 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { toast } from '@/components/ui/use-toast'
+
+const FormSchema = z.object({
+  email: z
+    .string({
+      required_error: 'El email es requerido.',
+      invalid_type_error: 'El email debe ser una cadena.',
+    })
+    .trim()
+    .toLowerCase()
+    .min(1, { message: 'Debes introducir un email' })
+    .email({ message: 'Email inválido.' }),
+  password: z
+    .string({
+      required_error: 'La contraseña es requerida.',
+      invalid_type_error: 'La contraseña debe ser una cadena.',
+    })
+    .min(1, { message: 'Debes introducir una contraseña' }),
+})
+
+export function LoginForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  function handleSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: 'You submitted the following values:',
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    })
+  }
+
+  return (
+    <Form {...form}>
+      <form className="grid gap-4" onSubmit={form.handleSubmit(handleSubmit)}>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input
+                  type="email"
+                  placeholder="m@ejemplo.com"
+                  autoComplete="email"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Contraseña</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="********" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full">
+          Iniciar sesión
+        </Button>
+      </form>
+    </Form>
+  )
+}

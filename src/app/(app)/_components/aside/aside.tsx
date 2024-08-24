1510ac64cd60bd060b288b8513a45c49f7dfc368
lbavendanos <lbavendanos@gmail.com>
2024-08-24 15:01:27 -0500

    Fix aside and header

--- src/app/(app)/_components/aside/aside.tsx
+++ src/app/(app)/_components/aside/aside.tsx
@@ -41,14 +41,14 @@ export function Aside() {
               className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
             >
               <Home className="h-4 w-4" />
-              Home
+              Inicio
             </Link>
             <Link
               href="#"
               className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
             >
               <ShoppingCart className="h-4 w-4" />
-              Orders
+              Pedidos
               <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
                 6
               </Badge>
@@ -58,21 +58,21 @@ export function Aside() {
               className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
             >
               <Package className="h-4 w-4" />
-              Products{' '}
+              Productos
             </Link>
             <Link
               href="#"
               className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
             >
               <Users className="h-4 w-4" />
-              Customers
+              Clientes
             </Link>
             <Link
               href="#"
               className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
             >
               <LineChart className="h-4 w-4" />
-              Analytics
+              Estadísticas
             </Link>
           </nav>
         </div>
--- src/app/(app)/_components/header/header.tsx
+++ src/app/(app)/_components/header/header.tsx
@@ -47,14 +47,14 @@ export function Header() {
               className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
             >
               <Home className="h-5 w-5" />
-              Home
+              Inicio
             </Link>
             <Link
               href="#"
               className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
             >
               <ShoppingCart className="h-5 w-5" />
-              Orders
+              Pedidos
               <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
                 6
               </Badge>
@@ -64,21 +64,21 @@ export function Header() {
               className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
             >
               <Package className="h-5 w-5" />
-              Products
+              Productos
             </Link>
             <Link
               href="#"
               className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
             >
               <Users className="h-5 w-5" />
-              Customers
+              Clientes
             </Link>
             <Link
               href="#"
               className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
             >
               <LineChart className="h-5 w-5" />
-              Analytics
+              Estadísticas
             </Link>
           </nav>
           <div className="mt-auto">

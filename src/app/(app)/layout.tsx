d185fb749ed3a8984fbc50c96e65e8241b00e27e
lbavendanos <lbavendanos@gmail.com>
2024-11-13 12:41:39 -0500

    Add sidebar

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,8 +1,21 @@
 'use client'
 
 import { useAuth } from '@/core/auth'
-import { Header } from './_components/app/header/header'
-import { Aside } from './_components/app/aside/aside'
+import { AppSidebar } from './_components/app/app-sidebar'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb'
+import { Separator } from '@/components/ui/separator'
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar'
 import { AppLoading } from './_components/app/app-loading'
 import { AppUnauthenticated } from './_components/app/app-unauthenticated'
 import { AppUnverified } from './_components/app/app-unverified'
@@ -19,14 +32,38 @@ export default function AppLayout({
   if (check && !user?.email_verified_at) return <AppUnverified />
 
   return (
-    <div className="flex min-h-screen w-full flex-col">
-      <Aside />
-      <div className="flex grow flex-col md:pl-56 lg:pl-72">
-        <Header />
-        <main className="flex grow bg-muted/40 pt-14 lg:pt-[60px]">
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
           {children}
-        </main>
-      </div>
-    </div>
+          {/* <div className="grid auto-rows-min gap-4 md:grid-cols-3"> */}
+          {/*   <div className="aspect-video rounded-xl bg-muted/50" /> */}
+          {/*   <div className="aspect-video rounded-xl bg-muted/50" /> */}
+          {/*   <div className="aspect-video rounded-xl bg-muted/50" /> */}
+          {/* </div> */}
+          {/* <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" /> */}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   )
 }
---
+++ src/app/(app)/old-layout.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { useAuth } from '@/core/auth'
+import { Header } from './_components/app/header/header'
+import { Aside } from './_components/app/aside/aside'
+import { AppLoading } from './_components/app/app-loading'
+import { AppUnauthenticated } from './_components/app/app-unauthenticated'
+import { AppUnverified } from './_components/app/app-unverified'
+
+export default function AppLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  const { check, user, isLoading } = useAuth()
+
+  if (isLoading) return <AppLoading />
+
+  if (!check) return <AppUnauthenticated />
+
+  if (check && !user?.email_verified_at) return <AppUnverified />
+
+  return (
+    <div className="flex min-h-screen w-full flex-col">
+      <Aside />
+      <div className="flex grow flex-col md:pl-56 lg:pl-72">
+        <Header />
+        <main className="flex grow bg-muted/40 pt-14 lg:pt-[60px]">
+          {children}
+        </main>
+      </div>
+    </div>
+  )
+}

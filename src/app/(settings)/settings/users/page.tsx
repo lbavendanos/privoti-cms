89c710f80fdf19006169c3b78c6ef6e28229900f
lbavendanos <lbavendanos@gmail.com>
2024-12-08 20:13:03 -0500

    Add users page

--- src/app/(settings)/settings/users/page.tsx
+++ src/app/(settings)/settings/users/page.tsx
@@ -1,7 +1,7 @@
 export default function AccountPage() {
   return (
     <div>
-      <h1>Settings Account</h1>
+      <h1>Settings Users</h1>
       <p>This is the settings page.</p>
     </div>
   )

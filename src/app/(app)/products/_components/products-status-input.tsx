a0739c899fdc1c0afb699b7e00d5db8cd8c2fe50
lbavendanos <lbavendanos@gmail.com>
2025-03-07 20:56:53 -0500

    Fix status input

--- src/app/(app)/products/_components/products-status-input.tsx
+++ src/app/(app)/products/_components/products-status-input.tsx
@@ -32,10 +32,14 @@ type ProductsStatusInputProps = {
 export function ProductsStatusInput({
   value,
   onChange,
+  ...props
 }: ProductsStatusInputProps) {
   return (
     <Select onValueChange={onChange} defaultValue={value} value={value}>
-      <SelectTrigger className="[&>span]:flex [&>span]:items-center [&>span]:gap-2 [&>span_svg]:shrink-0">
+      <SelectTrigger
+        {...props}
+        className="[&>span]:flex [&>span]:items-center [&>span]:gap-2 [&>span_svg]:shrink-0"
+      >
         <SelectValue placeholder="Select status" />
       </SelectTrigger>
       <SelectContent className="[&_*[role=option]>span>svg]:shrink-0 [&_*[role=option]>span>svg]:text-muted-foreground/80 [&_*[role=option]>span]:end-2 [&_*[role=option]>span]:start-auto [&_*[role=option]>span]:flex [&_*[role=option]>span]:items-center [&_*[role=option]>span]:gap-2 [&_*[role=option]]:pe-8 [&_*[role=option]]:ps-2">

52eba32e22e0abdbf980297e2cfa4c75b71947ca
lbavendanos <lbavendanos@gmail.com>
2025-03-13 15:36:19 -0500

    Fix next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   experimental: {
+    dynamicIO: true,
     useCache: true,
     serverActions: {
       bodySizeLimit: '2mb',

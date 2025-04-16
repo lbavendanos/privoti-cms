a11eb8fc101c09c73f74c10a213a137df2f810da
lbavendanos <lbavendanos@gmail.com>
2025-04-16 11:24:44 -0500

    feat(ui): refactor StatusAlert to use Alert component
    Refactored the StatusAlert component to utilize the Alert component for
    better modularity and maintainability. Replaced the custom icon logic
    with direct usage of icons and updated styles for consistency. Removed
    redundant code and improved readability.

--- src/components/ui/status-alert.tsx
+++ src/components/ui/status-alert.tsx
@@ -1,9 +1,10 @@
 import { cn } from '@/lib/utils'
+import { Alert, AlertDescription, AlertTitle } from './alert'
 import {
   InfoIcon,
   TriangleAlert,
   CircleCheckIcon,
-  CircleAlertIcon,
+  AlertCircleIcon,
 } from 'lucide-react'
 
 type CustomAlertProps = {
@@ -21,74 +22,35 @@ export function StatusAlert({
   list,
   className,
 }: CustomAlertProps) {
-  const getIcon = () => {
-    const iconProps = {
-      className: 'mt-0.5 shrink-0',
-      size: 16,
-      'aria-hidden': true,
-    }
-
-    switch (variant) {
-      case 'info':
-        return (
-          <InfoIcon
-            {...iconProps}
-            className={`${iconProps.className} text-blue-500`}
-          />
-        )
-      case 'success':
-        return (
-          <CircleCheckIcon
-            {...iconProps}
-            className={`${iconProps.className} text-emerald-500`}
-          />
-        )
-      case 'warning':
-        return (
-          <TriangleAlert
-            {...iconProps}
-            className={`${iconProps.className} text-amber-500`}
-          />
-        )
-      case 'error':
-        return (
-          <CircleAlertIcon
-            {...iconProps}
-            className={`${iconProps.className} text-red-500`}
-          />
-        )
-      default:
-        return null
-    }
-  }
-
   return (
-    <div
+    <Alert
       className={cn(
-        'flex gap-3 rounded-md border px-4 py-3',
-        variant === 'info' && 'border-blue-500/50 text-blue-600',
-        variant === 'success' && 'border-emerald-500/50 text-emerald-600',
-        variant === 'warning' && 'border-amber-500/50 text-amber-600',
-        variant === 'error' && 'border-red-500/50 text-red-600',
+        variant === 'info' &&
+          'bg-card border-blue-500 text-blue-500 *:data-[slot=alert-description]:text-blue-500/90 [&>svg]:text-current',
+        variant === 'success' &&
+          'bg-card border-emerald-500 text-emerald-500 *:data-[slot=alert-description]:text-emerald-500/90 [&>svg]:text-current',
+        variant === 'warning' &&
+          'bg-card border-amber-500 text-amber-500 *:data-[slot=alert-description]:text-amber-500/90 [&>svg]:text-current',
+        variant === 'error' &&
+          'text-destructive border-destructive bg-card *:data-[slot=alert-description]:text-destructive/90 [&>svg]:text-current',
         className,
       )}
     >
-      {getIcon()}
-      <div className="grow space-y-1">
-        {title && (
-          <h5 className="leading-none font-medium tracking-tight">{title}</h5>
-        )}
-        {description && (
-          <p className="text-sm leading-relaxed">{description}</p>
-        )}
+      {variant === 'info' && <InfoIcon />}
+      {variant === 'success' && <CircleCheckIcon />}
+      {variant === 'warning' && <TriangleAlert />}
+      {variant === 'error' && <AlertCircleIcon />}
+      {title && <AlertTitle>{title}</AlertTitle>}
+      <AlertDescription>
+        {description && <p>{description}</p>}
         {list && (
-          <ul className="text-muted-foreground list-inside list-disc text-sm">
+          <ul className="list-inside list-disc text-sm">
             {list.map((item) => (
               <li key={item}>{item}</li>
             ))}
           </ul>
         )}
-      </div>
-    </div>
+      </AlertDescription>
+    </Alert>
   )
 }
